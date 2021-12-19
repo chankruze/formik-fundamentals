fa8b89818838e042bcbf09889dad431857efc393
chankruze <chankruze@gmail.com>
2021-12-19 07:51:23 +0530

    manual validation: form level validation demo
    Signed-off-by: chankruze <chankruze@gmail.com>

--- src/YoutubeFormV2.js
+++ src/YoutubeFormV2.js
@@ -84,213 +84,243 @@ const YoutubeFormV2 = () => {
         // validateOnBlur={false}
         // validateOnChange={false}
       >
-        <Form className={styles.form}>
-          {/* name Field */}
-          <div className={styles.wrapper_div}>
-            <label htmlFor="name">Name</label>
-            <Field
-              type="text"
-              // name attribute is required
-              name="name"
-              id="name"
-              placeholder="Chadan Kumar Mandal"
-            />
-            <ErrorMessage name="name" component={ErrorComponent} />
-          </div>
-          {/* email Field */}
-          <div className={styles.wrapper_div}>
-            <label htmlFor="email">Email</label>
-            <Field
-              type="email"
-              // name attribute is required
-              name="email"
-              id="email"
-              placeholder="example@domain.xyz"
-            />
-            <ErrorMessage name="email" component={ErrorComponent} />
-          </div>
-          {/* channel Field */}
-          <div className={styles.wrapper_div}>
-            <label htmlFor="channel">Channel</label>
-            <Field
-              type="text"
-              // name attribute is required
-              name="channel"
-              id="channel"
-              placeholder="https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ"
-              // Field level validation
-              validate={validateField}
-            />
-            {/* field level validation */}
-            <ErrorMessage name="channel" component={ErrorComponent} />
-            {/* Demo using render props */}
-            {/* <ErrorMessage name="channel">
+        {(formik) => (
+          <Form className={styles.form}>
+            {/* name Field */}
+            <div className={styles.wrapper_div}>
+              <label htmlFor="name">Name</label>
+              <Field
+                type="text"
+                // name attribute is required
+                name="name"
+                id="name"
+                placeholder="Chadan Kumar Mandal"
+              />
+              <ErrorMessage name="name" component={ErrorComponent} />
+            </div>
+            {/* email Field */}
+            <div className={styles.wrapper_div}>
+              <label htmlFor="email">Email</label>
+              <Field
+                type="email"
+                // name attribute is required
+                name="email"
+                id="email"
+                placeholder="example@domain.xyz"
+              />
+              <ErrorMessage name="email" component={ErrorComponent} />
+            </div>
+            {/* channel Field */}
+            <div className={styles.wrapper_div}>
+              <label htmlFor="channel">Channel</label>
+              <Field
+                type="text"
+                // name attribute is required
+                name="channel"
+                id="channel"
+                placeholder="https://www.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ"
+                // Field level validation
+                validate={validateField}
+              />
+              {/* field level validation */}
+              <ErrorMessage name="channel" component={ErrorComponent} />
+              {/* Demo using render props */}
+              {/* <ErrorMessage name="channel">
               {(errorMsg) => (
                 <div className={styles.error_div}>
                   <p>{errorMsg}</p>
                 </div>
               )}
             </ErrorMessage> */}
-          </div>
-          {/* address Field (textarea) */}
-          {/* render props */}
-          <div className={styles.wrapper_div}>
-            <label htmlFor="address">Address</label>
-            <Field
-              name="address"
-              // Field level validation
-              validate={validateField}
-            >
-              {
-                // render props
-                ({ field, form, meta }) => {
+            </div>
+            {/* address Field (textarea) */}
+            {/* render props */}
+            <div className={styles.wrapper_div}>
+              <label htmlFor="address">Address</label>
+              <Field
+                name="address"
+                // Field level validation
+                validate={validateField}
+              >
+                {
+                  // render props
+                  ({ field, form, meta }) => {
+                    return (
+                      <div>
+                        <textarea
+                          id="address"
+                          placeholder="Cecilia Chapman&#10;711-2880 Nulla St.&#10;Mankato Mississippi 96522&#10;(257) 563-7401"
+                          {...field}
+                        />
+                        {meta.touched && meta.error && (
+                          <div className={styles.error_div}>
+                            <p>{meta.error}</p>
+                          </div>
+                        )}
+                      </div>
+                    );
+                  }
+                }
+              </Field>
+            </div>
+            {/* social */}
+            <div className={styles.group_wrapper}>
+              <div className={styles.group_heading}>
+                <div className={styles.group_bar}></div>
+                <p className={styles.group_title}>Social</p>
+                <div className={styles.group_bar}></div>
+              </div>
+              <div className={styles.handle_wrapper}>
+                <label htmlFor="facebook">
+                  <FaFacebook size={32} />
+                </label>
+                <Field
+                  type="text"
+                  name="social.facebook"
+                  id="facebook"
+                  placeholder="chankruze"
+                />
+              </div>
+              <div className={styles.handle_wrapper}>
+                <label htmlFor="twitter">
+                  <FaTwitter size={32} />
+                </label>
+                <Field
+                  type="text"
+                  name="social.twitter"
+                  id="twitter"
+                  placeholder="chankruze"
+                />
+              </div>
+              <div className={styles.handle_wrapper}>
+                <label htmlFor="instagram">
+                  <FaInstagram size={32} />
+                </label>
+                <Field
+                  type="text"
+                  name="social.instagram"
+                  id="instagram"
+                  placeholder="chankruze"
+                />
+              </div>
+              <div className={styles.handle_wrapper}>
+                <label htmlFor="github">
+                  <FaGithub size={32} />
+                </label>
+                <Field
+                  type="text"
+                  name="social.github"
+                  id="github"
+                  placeholder="chankruze"
+                />
+              </div>
+            </div>
+            {/* phone number fields */}
+            <div className={styles.group_wrapper}>
+              <div className={styles.group_heading}>
+                <div className={styles.group_bar}></div>
+                <p className={styles.group_title}>phone numbers</p>
+                <div className={styles.group_bar}></div>
+              </div>
+              <div className={styles.handle_wrapper}>
+                <label htmlFor="phone 1">
+                  <FaPhone size={32} />
+                </label>
+                <Field
+                  type="text"
+                  name="phoneNumbers[0]"
+                  id="primaryPhone"
+                  placeholder="0123456789"
+                />
+              </div>
+              <div className={styles.handle_wrapper}>
+                <label htmlFor="twitter">
+                  <FaPhoneAlt size={32} />
+                </label>
+                <Field
+                  type="text"
+                  name="phoneNumbers[1]"
+                  id="secondaryPhone"
+                  placeholder="0123456789"
+                />
+              </div>
+            </div>
+
+            <div className={styles.group_wrapper}>
+              <div className={styles.group_heading}>
+                <div className={styles.group_bar}></div>
+                <p className={styles.group_title}>hobbies</p>
+                <div className={styles.group_bar}></div>
+              </div>
+              <FieldArray name="hobbies">
+                {({
+                  push,
+                  remove,
+                  form: {
+                    values: { hobbies },
+                  },
+                }) => {
+                  console.log(hobbies);
                   return (
                     <div>
-                      <textarea
-                        id="address"
-                        placeholder="Cecilia Chapman&#10;711-2880 Nulla St.&#10;Mankato Mississippi 96522&#10;(257) 563-7401"
-                        {...field}
-                      />
-                      {meta.touched && meta.error && (
-                        <div className={styles.error_div}>
-                          <p>{meta.error}</p>
+                      {hobbies.map((hobby, index) => (
+                        <div key={index} className={styles.hobby_wrapper}>
+                          <Field name={`hobbies[${index}]`} />
+                          <button type="button" onClick={() => remove(index)}>
+                            <FaRegTimesCircle size={24} />
+                          </button>
                         </div>
-                      )}
-                    </div>
-                  );
-                }
-              }
-            </Field>
-          </div>
-          {/* social */}
-          <div className={styles.group_wrapper}>
-            <div className={styles.group_heading}>
-              <div className={styles.group_bar}></div>
-              <p className={styles.group_title}>Social</p>
-              <div className={styles.group_bar}></div>
-            </div>
-            <div className={styles.handle_wrapper}>
-              <label htmlFor="facebook">
-                <FaFacebook size={32} />
-              </label>
-              <Field
-                type="text"
-                name="social.facebook"
-                id="facebook"
-                placeholder="chankruze"
-              />
-            </div>
-            <div className={styles.handle_wrapper}>
-              <label htmlFor="twitter">
-                <FaTwitter size={32} />
-              </label>
-              <Field
-                type="text"
-                name="social.twitter"
-                id="twitter"
-                placeholder="chankruze"
-              />
-            </div>
-            <div className={styles.handle_wrapper}>
-              <label htmlFor="instagram">
-                <FaInstagram size={32} />
-              </label>
-              <Field
-                type="text"
-                name="social.instagram"
-                id="instagram"
-                placeholder="chankruze"
-              />
-            </div>
-            <div className={styles.handle_wrapper}>
-              <label htmlFor="github">
-                <FaGithub size={32} />
-              </label>
-              <Field
-                type="text"
-                name="social.github"
-                id="github"
-                placeholder="chankruze"
-              />
-            </div>
-          </div>
-          {/* phone number fields */}
-          <div className={styles.group_wrapper}>
-            <div className={styles.group_heading}>
-              <div className={styles.group_bar}></div>
-              <p className={styles.group_title}>phone numbers</p>
-              <div className={styles.group_bar}></div>
-            </div>
-            <div className={styles.handle_wrapper}>
-              <label htmlFor="phone 1">
-                <FaPhone size={32} />
-              </label>
-              <Field
-                type="text"
-                name="phoneNumbers[0]"
-                id="primaryPhone"
-                placeholder="0123456789"
-              />
-            </div>
-            <div className={styles.handle_wrapper}>
-              <label htmlFor="twitter">
-                <FaPhoneAlt size={32} />
-              </label>
-              <Field
-                type="text"
-                name="phoneNumbers[1]"
-                id="secondaryPhone"
-                placeholder="0123456789"
-              />
-            </div>
-          </div>
-
-          <div className={styles.group_wrapper}>
-            <div className={styles.group_heading}>
-              <div className={styles.group_bar}></div>
-              <p className={styles.group_title}>hobbies</p>
-              <div className={styles.group_bar}></div>
-            </div>
-            <FieldArray name="hobbies">
-              {({
-                push,
-                remove,
-                form: {
-                  values: { hobbies },
-                },
-              }) => {
-                console.log(hobbies);
-                return (
-                  <div>
-                    {hobbies.map((hobby, index) => (
-                      <div key={index} className={styles.hobby_wrapper}>
-                        <Field name={`hobbies[${index}]`} />
-                        <button type="button" onClick={() => remove(index)}>
-                          <FaRegTimesCircle size={24} />
+                      ))}
+                      <div>
+                        <button
+                          type="button"
+                          onClick={() => push("")}
+                          className={styles.btn_add_hobby}
+                        >
+                          <FaPlus size={16} />
+                          <p>Add hobby</p>
                         </button>
                       </div>
-                    ))}
-                    <div>
-                      <button
-                        type="button"
-                        onClick={() => push("")}
-                        className={styles.btn_add_hobby}
-                      >
-                        <FaPlus size={16} />
-                        <p>Add hobby</p>
-                      </button>
                     </div>
-                  </div>
-                );
-              }}
-            </FieldArray>
-          </div>
+                  );
+                }}
+              </FieldArray>
+            </div>
 
-          {/* submit button */}
-          <div className={styles.wrapper_div}>
-            <button type="submit">Submit</button>
-          </div>
-        </Form>
+            {/* validation buttons */}
+            <div className={styles.validation_div}>
+              {/* validate field */}
+              <button
+                type="button"
+                onClick={() => {
+                  formik.setFieldTouched("channel");
+                  formik.validateField("channel");
+                }}
+              >
+                validate channel
+              </button>
+              {/* validate all */}
+              <button
+                type="button"
+                onClick={() => {
+                  formik.setTouched({
+                    name: true,
+                    email: true,
+                    channel: true,
+                    address: true,
+                  });
+                  formik.validateForm();
+                }}
+              >
+                validate form
+              </button>
+            </div>
+            {/* submit button */}
+            <div className={styles.wrapper_div}>
+              <button type="submit">Submit</button>
+            </div>
+          </Form>
+        )}
       </Formik>
     </div>
   );
